3dd1b54b1ef453efe9c9fae710d4ad893fea8a99
eloobrandao <eloisabrandao210@gmail.com>
2025-11-14 11:32:13 -0300

    Add files via upload

---
+++ Vetores15/Exercicio6.java
@@ -0,0 +1,36 @@
+package exComplVetores;
+
+import java.util.Scanner;
+
+public class Exercicio6 {
+
+	public static void main(String[] args) {
+		Scanner in = new Scanner (System.in);
+		final int TAM= 5;
+		int i, j,a[], potencia=1;
+		
+		a = new int[TAM];
+		
+		for(i=0; i<TAM;i++) {
+			System.out.println("Digite o "+ (i+1)+ "º valor");
+			a[i]= in.nextInt();
+			
+		}
+		
+		for(i=0; i<TAM; i++) {
+			potencia=1;
+			for(j=1; j<=a[i]; j++) {
+			potencia = potencia *2; 
+		   }
+			a[i]=potencia;
+		}
+			
+			System.out.print("Vetor A = [");
+	        for ( i = 0; i < TAM; i++) {
+	            System.out.print(a[i]+ ",");
+	        }System.out.print("]");
+		
+
+	}
+
+}
